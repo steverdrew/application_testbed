e4b0247a77aa6ca79468d981d252611979eaeaa2
Steve <steverdrew@gmail.com>
2013-08-11 13:08:13 +0100

    search page starting to take shape

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,16 +14,8 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require foundation
+
 $(document).foundation();
-
-wrapSelects = function() {
-  $('form.awesome select:not(.is-wrapped)')
-    .addClass('is-wrapped')
-    .wrap('<div class="custom dropdown select" />')
-    .after('<span class="selector"></span>');
-}
-
-$(wrapSelects);
 
 $(function() {
   $( "#startdate" ).datepicker();
@@ -32,4 +24,36 @@ $(function() {
 $(function() {
   $( "#enddate" ).datepicker();
   });
+  
+$(function initialize() {
+    var myLatlng = new google.maps.LatLng(54.29454,-1.98342);
+    var mapOptions = {
+      zoom: 4,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+    var contentString = '<h5 class="subheader">United Kingdom</h5>'+
+	'<p>Roughly 5,000 properties</p>'+
+	'<p><a href"foo" id="foo" data-id="1000">Click</a> to zoom in...</p>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'United Kingdon (~5,000 properties)'
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+	
+    });
+  	
+    google.maps.event.addDomListener(window, 'load', initialize);
+  
+  });
+
+  
