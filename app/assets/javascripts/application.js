be3bd3d1aab8501825057b898b5b30d5dfb2b31e
Steve <steverdrew@gmail.com>
2013-09-07 14:01:28 +0100

    search form rework

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,5 +40,9 @@ $(function() {
      scrollToAnchor('owner');
   });
 
-$(".chosen-select").chosen()
-   
+$(".chosen-select").chosen();
+
+$(window).load(function() { // makes sure the whole site is loaded
+	$("#status").fadeOut(); // will first fade out the loading animation
+	$("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
+})
